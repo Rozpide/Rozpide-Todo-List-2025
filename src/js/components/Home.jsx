ca809f0dbb678ee0b0b243265676b7641c1131ef
Rozpi <100348581+Rozpide@users.noreply.github.com>
2025-01-12 11:34:36 +0000

    agrego funciones para el manejo de la lista de tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,42 @@
-import React, { useState } from "react";
+import React, { useState } from "react";// importamos react y el hook de estado
 
 //create your first component
+// Inicializamos el estado para almacenar las tareas y la nueva tarea que se va a agregar
 const Home = () => {
-	const [tareas, setTareas]= useState([])
-	const [nuevaTarea, setNuevaTarea]= useState('')
+	const [tareas, setTareas]= useState([]);// 'tareas' es un arreglo que contendra la lista de tareas
+	const [nuevaTarea, setNuevaTarea]= useState(''); //'nuevaTarea' es una cadena vacia que se almacenara
+	// funcion para manejar los cambios en el input
 	const manejarEntrada = (e) => {
-		setNuevaTarea(e.target.value);
+		setNuevaTarea(e.target.value);// actualizo el estado de 'nuevaTarea?
 	}
+	// funcion para añadir una nueva tarea a la lista de tareas
 	const añadirTareas = () => {
-		if (nuevaTarea.trim() !== ''){
-			setTareas([...tareas, nuevaTarea]);
-			setNuevaTarea('');
+		if (nuevaTarea.trim() !== ''){ // verifico que 'nuevaTarea' no este vacia
+			setTareas([...tareas, nuevaTarea]);// añado la nueva tarea a 'tareas'
+			setNuevaTarea('');// despues limpiamos en campo del input
 		}
 	}
+	// funcion para eliminar una tarea especifica
 	const eliminarTarea = (index) => {
-		const modificarTareas = tareas.filter(())
-
+		const actualizarTareas = tareas.filter((_, i) => i !== index);// filtro la lista para eliminar la tarea por indice
+		setTareas(actualizarTareas);// actualizo el estado de 'tareas' con la lista filtrada
+		
 	}
 	return (
 		<>
-		<input type="text" onChange={añadirTarea}/>
-		<button onClick={guardarTarea}>añade la tarea</button>
+			<h1>Lista de Tareas</h1>
+			<div>
+				<input type="text" value={nuevaTarea} onChange={manejarEntrada} placeholder="Escribe una nueva tarea" />
+				<button onClick={añadirTareas}>Añade aqui nueva tarea</button>
+			</div>
+			<ul>
+			{tareas.map((tarea, index) => (
+				<li key={index}>
+					{tarea}
+					<button onClick={() => eliminarTarea(index)}>Eliminar</button>
+				</li>
+			))}
+			</ul>
 		</>
 	);
 };
